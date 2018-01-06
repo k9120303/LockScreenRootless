04218535db917a62fa51217bc93cb024f86118fe
k9120303 <k9120303@gmail.com>
2018-01-06 23:03:40 +0800

    2018/1/6 ver1.01

---
+++ app/src/main/java/g7/lockscreenrootless/deviceAdminReceiver.java
@@ -0,0 +1,11 @@
+package g7.lockscreenrootless;
+
+/*
+ * Created by Administrator on 2017/12/26.
+ */
+
+import android.app.admin.DeviceAdminReceiver;
+// 取得系統的裝置管理員權限
+public class deviceAdminReceiver extends DeviceAdminReceiver {
+
+}
